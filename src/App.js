a74269b70e444f7cec2ee54dce31e0a90c592ae2
Hamed Feizabadi <hamedfz19@gmail.com>
2021-08-10 22:10:43 +0430

    add loading bar

--- src/App.js
+++ src/App.js
@@ -30,13 +30,20 @@ class App extends React.Component {
 				return robot.name.toLowerCase().includes(this.state.searchField.toLowerCase());
 			});
 
-		return(
-			<div className='tc'>
-				<h1 className="f1">RoboFriends</h1>
-				<SearchBox searchChange={this.onSearchChange}/>
-				<CardList robots={filteredRobots}/>
-			</div>
-		);
+		if(this.state.robots.length == 0)
+		{
+			return ( <h1 className="tc">Loading</h1> );
+		}
+		else
+		{
+			return(
+				<div className='tc'>
+					<h1 className="f1">RoboFriends</h1>
+					<SearchBox searchChange={this.onSearchChange}/>
+					<CardList robots={filteredRobots}/>
+				</div>
+			);
+		}
 	}
 }
 
