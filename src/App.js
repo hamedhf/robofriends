212fc718bbcb11e536ef111b6b8fd8cefd10c63d
Hamed Feizabadi <hamedfz19@gmail.com>
2021-08-10 21:41:38 +0430

    some styling + fetching date
    fonts add, code refactor, fetch date, css update

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import SearchBox from './SearchBox';
 import CardList from './CardList';
-import { robots } from './robots';
+import './App.css';
 
 
 class App extends React.Component {
@@ -9,8 +9,16 @@ class App extends React.Component {
 		super();
 		this.state = {
 			searchField: '',
-			robots: robots
+			robots: []
 		}
+	}
+
+	componentDidMount(){
+		fetch('https://jsonplaceholder.typicode.com/users')
+			.then(response => response.json())
+			.then(users => {
+				this.setState({ robots: users });
+			});
 	}
 
 	onSearchChange = (event) => {
@@ -24,7 +32,7 @@ class App extends React.Component {
 
 		return(
 			<div className='tc'>
-				<h1>RoboFriends</h1>
+				<h1 className="f1">RoboFriends</h1>
 				<SearchBox searchChange={this.onSearchChange}/>
 				<CardList robots={filteredRobots}/>
 			</div>
